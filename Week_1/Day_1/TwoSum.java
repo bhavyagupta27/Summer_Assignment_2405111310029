06d30250d3a83ad642b723bf1c91797f789ae080
Bhavya Gupta <bhavyagupta2710@gmail.com>
2026-06-05 09:49:42 +0530

    Completetd Day 1 Week 1

--- Week_1/Day_1/TwoSum.java
+++ Week_1/Day_1/TwoSum.java
@@ -1,5 +1,6 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+import java.util.*;
+class TwoSum {
+    public int[] TwoSum(int[] nums, int target) {
          HashMap<Integer, Integer> map = new HashMap<>();
         
         for(int i = 0; i < nums.length; i++) {
